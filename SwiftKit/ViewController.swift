cae606cb7f896019e0590858eb53a7d7d7016de3
马光然 <maguangran@youshehui.com>
2018-04-24 09:53:53 +0800

    添加基础框架

---
+++ SwiftKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftKit
+//
+//  Created by 马光然 on 2018/4/24.
+//  Copyright © 2018年 马光然. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
